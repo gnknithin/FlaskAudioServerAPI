260e277668974285aa0a162ca9e38d14757cd517
Nithin <gnknithin@gmail.com>
2021-03-03 12:26:12 +0530

    Submit

---
+++ app/api/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from flask import Blueprint
+except ModuleNotFoundError:
+    print("\nFlask must be Installed\n")
+    sys.exit(0)
+
+api = Blueprint('api',__name__)
+
+from . import views, errors
